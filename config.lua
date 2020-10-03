51e4375d9d6f4de3bc4fed44d5979e7a4df0e930
Doxylamin <Doxylamin@users.noreply.github.com>
2020-10-03 23:57:45 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -17,7 +17,7 @@ Config.RestrictedChannels = {
 
 Config.messages = {
   ['not_on_radio'] = 'Du bist aktuell mit keinem Funkkanal verbunden.',
-  ['on_radio'] = 'Aktive Funkverbindung: <b>%s</b>',
+  ['on_radio'] = 'Funkverbindung aktiv: <b>%s</b>',
   ['joined_to_radio'] = 'Du bist dem Funkkanal <b>%s</b>.00 MHz beigetreten.',
   ['restricted_channel_error'] = 'Du kannst dem verschlüsselten Funkkanal <b>%s</b>.00 MHz nicht beitreten!',
   ['you_on_radio'] = 'Du bist bereits im <b>%s</b>.00 MHz Funkkanal.',
